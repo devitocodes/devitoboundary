ac6c554f177c20a4eee22ff805f89403d7d15d5b
EdCaunt <ec815@ic.ac.uk>
2020-03-18 15:02:50 +0000

    Experiments with k-d tree indexing

--- devitoboundary/structures.py
+++ devitoboundary/structures.py
@@ -21,6 +21,9 @@ class PolyMesh():
         Array of surface coordinates grouped as [[x, y, z], [x, y, z], ...]
     grid : Devito Grid object
         The grid against which the boundary surface is to be defined
+    setup : bool, optional
+        Set up the Delaunay triangulation and parameterize surface immediately.
+        Set to 'False' if you want to combine several topography datasets.
 
     Attributes
     ----------
@@ -36,17 +39,17 @@ class PolyMesh():
         The constant of the plane equation
     """
 
-    def __init__(self, data, grid):
+    def __init__(self, data, grid, setup=True):
         assert len(grid.dimensions) == 3, "PolyMesh is for 3D grids only"
         assert len(np.shape(data)) == 2, "Coordinates for each point should be in the form [x, y, z]"
 
+        self._grid = grid
         self._points = np.array(data, dtype=np.float32)
-        # Initialise properties
-        self._core_properties()
-        # Characterize planes
-        self._characterise_planes()
-        # Set up K-d tree
-        self._tree_setup()
+        self._points /= self._grid.spacing  # Convert to grid-index space
+
+        self._setup_bool = False
+        if setup:
+            self.setup()
 
     @property
     def points(self):
@@ -78,9 +81,36 @@ class PolyMesh():
         """The constant of the plane equation"""
         return self._values
 
+    def add_data(self, data):
+        """
+        Add additional topography data to the mesh prior to setup.
+
+        Parameters
+        ----------
+        data : array_like
+            Array of surface coordinates grouped as [[x, y, z], [x, y, z], ...]
+        """
+        assert self._setup_bool is False, "Topography cannot be added after mesh setup."
+        add_points = np.array(data, dtype=np.float32)
+        add_points /= self._grid.spacing  # Convert to grid-index space
+        self._points = np.concatenate(self._points, add_points)
+
+    def setup(self):
+        """Set up the mesh. The topography dataset cannot be modified once called."""
+        assert self._setup_bool is False, "Mesh setup has already taken place."
+        self._setup_bool = True  # Prevents repeated mesh setups
+        # Initialise properties
+        self._core_properties()
+        # Characterize planes
+        self._characterise_planes()
+        # Set up K-d tree
+        self._tree_setup()
+        # Set up lookup table for polygons
+        self._vertex_to_simplices()
+
     def _core_properties(self):
         """Set up all the core properties of the mesh"""
-        mesh = Delaunay(self._points[:, :2])
+        mesh = Delaunay(self._points[:, :2], qhull_options="QJ")
         self._simplices = mesh.simplices
         self._neighbors = mesh.neighbors
         self._polycount = len(self._simplices)
@@ -104,6 +134,16 @@ class PolyMesh():
         """
         self._tree = cKDTree(self._points)
 
+    def _vertex_to_simplices(self):
+        """
+        Initialise lookup table for finding all the simplices associated with a given
+        vertex.
+        """
+        self._simplex_lookup = [() for vertex in self._points]  # Set up our lookup table
+
+        for i in range(self._simplices.size):
+            self._simplex_lookup[self._simplices.flat[i]] += (int(i/3),)
+
     def query_points(self, x, k, distance_upper_bound=np.inf):
         """
         Query the tree of surface points.
@@ -124,6 +164,7 @@ class PolyMesh():
         i : ndarray
             Integers of nearest neighbors in 'points'
         """
+        assert self._setup_bool is True, "The mesh has not been set up."
 
         return self._tree.query(x, k, eps=np.finfo(np.float32).eps,
                                 distance_upper_bound=distance_upper_bound,
@@ -147,7 +188,8 @@ class PolyMesh():
         i : ndarray
             Indices of attatched polygons in 'simplices'
         """
+        assert self._setup_bool is True, "The mesh has not been set up."
 
         _, indices = self.query_points(x, k, distance_upper_bound)
-        return indices
-        # Might need a python-level loop here
+
+        return [self._simplex_lookup[index] for index in indices]
--- wip_scripts/structures_testing.py
+++ wip_scripts/structures_testing.py
@@ -1,16 +1,19 @@
 from devito import Grid
 from devitoboundary import PolyMesh
+import numpy as np
 
 data = [[1, 2, 3], [2, 1, 0], [5, 1, 2],
         [5, 3, 1], [1, 3, 5], [1, 2, 8],
         [2, 5, 7], [4, 1, 2], [8, 5, 2],
-        [17, 15, 1], [13, 15, 2], [13, 14, 3]]
+        [17, 15, 1], [13, 15, 2], [13, 14, 3],
+        [1.3, 3.5, 2], [4.8, 2, 4], [2.6, 3, 2]]
 
 grid = Grid(extent=(20, 20, 20), shape=(11, 11, 11))
 
 mesh = PolyMesh(data, grid)
 
 qpoint = [4.2, 2.2, 5.3]
-print(mesh.simplices)
+print(mesh.simplices, "\n")
 
-print(mesh.query_polygons(qpoint, 3))
+polys = mesh.query_polygons(qpoint, 3)
+print(polys)
