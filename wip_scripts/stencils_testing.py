91a97d533fd7610b2f517d2af353b0b034745f41
EdCaunt <ec815@ic.ac.uk>
2020-08-05 14:28:47 +0100

    Replaced sp Eqs with devito Eqs

--- wip_scripts/stencils_testing.py
+++ wip_scripts/stencils_testing.py
@@ -1,14 +1,15 @@
 import sympy as sp
 from devitoboundary import Stencil_Gen
+from devito import Eq
 
 s_o = 4
 n_pts = int(s_o/2)
 
 ext = Stencil_Gen(s_o)
 
-bc_0 = sp.Eq(ext.u(ext.x_b), 0)
-bc_2 = sp.Eq(ext.u(ext.x_b, 2), 0)
-bc_4 = sp.Eq(ext.u(ext.x_b, 4), 0)
+bc_0 = Eq(ext.u(ext.x_b), 0)
+bc_2 = Eq(ext.u(ext.x_b, 2), 0)
+bc_4 = Eq(ext.u(ext.x_b, 4), 0)
 bcs = [bc_0, bc_2, bc_4]
 
 ext.add_bcs(bcs)
