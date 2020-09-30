e2540270ed8663fe210147456db145d4fe92df32
EdCaunt <ec815@ic.ac.uk>
2020-09-30 10:51:20 +0100

    Corrected distance calculation signs

--- devitoboundary/distance.py
+++ devitoboundary/distance.py
@@ -6,10 +6,10 @@ distance functions discretized to a Devito grid.
 import sympy as sp
 
 from devito import Function, VectorFunction, grad, ConditionalDimension, \
-    Le, Gr, Eq, Operator
+    Le, Gt, Eq, Operator
 from devitoboundary import SDFGenerator
 
-__all__ = ['SignedDistanceFunction', 'AxialSignedDistanceFunction']
+__all__ = ['SignedDistanceFunction', 'AxialDistanceFunction']
 
 
 class SignedDistanceFunction:
@@ -37,7 +37,7 @@ class SignedDistanceFunction:
     grid : Devito Grid
         The grid onto which the signed distance function is discretized.
     """
-    def __init__(self, functions, infile, toggle_normals):
+    def __init__(self, functions, infile, toggle_normals=False):
         # Check variable type
         is_tuple = isinstance(functions, tuple)
         is_function = issubclass(type(functions), Function) \
@@ -67,7 +67,7 @@ class SignedDistanceFunction:
         self._sdf.data[:] = self._sdfgen.array
 
     @property
-    def function(self):
+    def sdf(self):
         """Get the absolute signed distance function."""
         return self._sdf
 
@@ -77,7 +77,7 @@ class SignedDistanceFunction:
         return self._grid
 
 
-class AxialSignedDistanceFunction(SignedDistanceFunction):
+class AxialDistanceFunction(SignedDistanceFunction):
     """
     A class to carry out the calculation of signed distances along each axis
     from a boundary surface represented by a point cloud or polygon file within
@@ -102,7 +102,7 @@ class AxialSignedDistanceFunction(SignedDistanceFunction):
     grid : Devito Grid
         The grid onto which the signed distance function is discretized.
     """
-    def __init__(self, functions, infile, toggle_normals):
+    def __init__(self, functions, infile, toggle_normals=False):
         super().__init__(functions, infile, toggle_normals)
 
         # Will need some kind of conditional dimension.
@@ -118,40 +118,60 @@ class AxialSignedDistanceFunction(SignedDistanceFunction):
         Update the axial distance function from the signed distance function.
         """
         # Set default values for axial distance
-        self._axial[0].data[:] = -self._order*self._grid.spacing[0]
-        self._axial[1].data[:] = -self._order*self._grid.spacing[1]
-        self._axial[2].data[:] = -self._order*self._grid.spacing[2]
+        # self._axial[0].data[:] = -self._order*self._grid.spacing[0]
+        # self._axial[1].data[:] = -self._order*self._grid.spacing[1]
+        # self._axial[2].data[:] = -self._order*self._grid.spacing[2]
 
         # Equations to decompose distance into axial distances
         x, y, z = self._grid.dimensions
         h_x, h_y, h_z = self._grid.spacing
         pos = sp.Matrix([x*h_x, y*h_y, z*h_z])
 
-        sdf_grad = grad(self._sdf)  # Gradient of the sdf
+        sdf_grad = grad(self._sdf).evaluate  # Gradient of the sdf
+
         # a*x + b*y + c*z = d
         a = sdf_grad[0]
         b = sdf_grad[1]
         c = sdf_grad[2]
-        d = sdf_grad.dot(pos + self._sdf*sdf_grad)
+        # d = sdf_grad.dot(pos + self._sdf*sdf_grad)
+        d = sdf_grad.dot(pos - self._sdf*sdf_grad)
 
         # Only need to calculate where SDF <= dx*M/2 (otherwise out of AOE)
-        close_sdf = Le(self._sdf, h_x*self._order/2)
+        # close_sdf = Le(self._sdf, h_x*self._order/2)
+        close_sdf = Le(sp.Abs(self._sdf), h_x*self._order/2)
         # If any of these are very small, then distance is very large
-        close_x = Gr(sp.Abs(a), 1e-6)
-        close_y = Gr(sp.Abs(b), 1e-6)
-        close_z = Gr(sp.Abs(c), 1e-6)
+        close_x = Gt(sp.Abs(a), 1e-6)
+        close_y = Gt(sp.Abs(b), 1e-6)
+        close_z = Gt(sp.Abs(c), 1e-6)
 
         # Conditional mask for calculation
-        mask_x = ConditionalDimension(name='mask_x', parent=y,
-                                      condition=sp.And(close_sdf, close_x))
-        mask_y = ConditionalDimension(name='mask_y', parent=y,
-                                      condition=sp.And(close_sdf, close_y))
-        mask_z = ConditionalDimension(name='mask_z', parent=y,
-                                      condition=sp.And(close_sdf, close_z))
+        # FIXME: Consolidate these
+        mask_x = ConditionalDimension(name='mask_x', parent=z,
+                                      condition=close_sdf)
+        mask_y = ConditionalDimension(name='mask_y', parent=z,
+                                      condition=close_sdf)
+        mask_z = ConditionalDimension(name='mask_z', parent=z,
+                                      condition=close_sdf)
 
-        eq_x = Eq(self._axial[0], (d - b*pos[1] - c*pos[2])/a, implicit_dims=mask_x)
-        eq_y = Eq(self._axial[1], (d - a*pos[0] - c*pos[2])/b, implicit_dims=mask_y)
-        eq_z = Eq(self._axial[2], (d - a*pos[0] - b*pos[1])/c, implicit_dims=mask_z)
+        mask_test = ConditionalDimension(name='mask_test', parent=z,
+                                         condition=close_sdf)
+
+        # Need some mins or something here.
+        eq_x = Eq(self._axial[0], (d - b*pos[1] - c*pos[2])/a - pos[0], implicit_dims=mask_x)
+        eq_y = Eq(self._axial[1], (d - a*pos[0] - c*pos[2])/b - pos[1], implicit_dims=mask_y)
+        eq_z = Eq(self._axial[2], (d - a*pos[0] - b*pos[1])/c - pos[2], implicit_dims=mask_z)
+
+        # eq_test = Eq(self._axial, sdf_grad, implicit_dims=mask_test)
+        # eq_test = Eq(self._axial, -self._sdf*sdf_grad, implicit_dims=mask_test)
+        # op_axial = Operator([eq_test])
+
+        # op_axial = Operator([eq_x])
 
         op_axial = Operator([eq_x, eq_y, eq_z], name='axial')
+        # print(op_axial.ccode)
         op_axial.apply()
+
+    @property
+    def axial(self):
+        """Get the axial distances"""
+        return self._axial
