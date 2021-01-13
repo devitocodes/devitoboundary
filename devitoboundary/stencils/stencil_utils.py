d5261ef765f690e77987ab326eb607ce4048c9b8
EdCaunt <ec815@ic.ac.uk>
2021-01-13 14:17:54 +0000

    Stencils: reversed effect of offset in standard_stencil

--- devitoboundary/stencils/stencil_utils.py
+++ devitoboundary/stencils/stencil_utils.py
@@ -26,7 +26,7 @@ def standard_stencil(deriv, space_order, offset=0., as_float=True):
         The stencil expression
     """
     # Want to start by calculating standard stencil expansions
-    min_index = offset - space_order/2
+    min_index = -offset - space_order/2
     x_list = [i + min_index for i in range(space_order+1)]
 
     base_coeffs = sp.finite_diff_weights(deriv, x_list, 0)[-1][-1]
